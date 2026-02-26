797e9ddf1242ba9f7aba9f6fe59591c78b7e4113
Altakie <artemlavrov@brandeis.edu>
2026-02-25 21:10:23 -0500

    Create table queries

--- app/routes/actors.py
+++ app/routes/actors.py
@@ -11,7 +11,9 @@ def view_all_actors():
     # >>>> TODO 1: Write a query that fetches all actors from `People` <<<<
     #              The query should retrieve `name`, `nationality`, `dob`, and `gender`.
 
-    query = """ """
+    query = """
+    SELECT * FROM ACTORS;
+    """
 
     with Database() as db:
         actors = db.execute(query=query)
